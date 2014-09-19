91fd4bd8d50d76a1ea1696e3c09416461356e649
Rohit Saxena <aerohit.saxena@gmail.com>
2014-09-19 13:12:15 +0200

    Create a client for communicating with the server protocol.

--- websocket_client.js
+++ websocket_client.js
@@ -1,31 +1,58 @@
-var my_socket = new WebSocket("ws:0.0.0.0:8125");
+function HookBase(server_url) {
+  var ws_connection = new WebSocket(server_url);
 
-my_socket.onopen = function (event) {
+  this.onOpen = function(callback) {
+    //ws_connection.onopen(callback);
+    ws_connection.onopen = callback;
+  };
+
+  this.onClose = function(callback) {
+    ws_connection.onclose = callback;
+  };
+
+  this.onMessage = function(callback) {
+    ws_connection.onmessage = callback;
+  };
+
+  this.onError = function(callback) {
+    ws_connection.onerror = callback;
+  };
+
+  this.sendString = function(data) {
+    ws_connection.send(data);
+  };
+
+  this.sendJS = function(data) {
+    ws_connection.send(JSON.stringify(data));
+  }
+
+  this.closeConnection = function() {
+    ws_connection.close();
+  };
+}
+
+var my_socket = new HookBase("ws:0.0.0.0:8125");
+
+my_socket.onOpen(function(event) {
   console.log("Connection open", event);
-  my_socket.send("Hello from client");
+  my_socket.sendString("Hello from client");
 
-  sendJS({
+  my_socket.sendJS({
     type: "message",
     text: "Sending some request",
     id:   "x123",
     date: Date.now()
   });
-};
+});
 
-my_socket.onmessage = function (event) {
+my_socket.onMessage(function(event) {
   console.log(event.data);
-};
+});
 
-my_socket.onerror = function (event) {
+my_socket.onError(function(event) {
   console.log("ERROR: ", event);
-};
+});
 
-my_socket.onclose = function (event) {
+my_socket.onClose(function(event) {
   console.log("Socket closed ", event);
-};
-
-function sendJS(data) {
-  my_socket.send(JSON.stringify(data));
-}
-
-
+});
