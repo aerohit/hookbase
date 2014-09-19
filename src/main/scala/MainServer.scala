7467173173d513a93a00e9f798b1e710042ff7a7
Rohit Saxena <aerohit.saxena@gmail.com>
2014-09-19 11:39:10 +0200

    Refactored server to handle all the websocket events.

--- src/main/scala/MainServer.scala
+++ src/main/scala/MainServer.scala
@@ -1,14 +1,36 @@
 import io.backchat.hookup._
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
 
 
 object MainServer extends App {
-  (HookupServer(8125) {
+  val server = HookupServer(8125) {
     new HookupServerClient {
       def receive = {
+        case c @ Connected =>
+          println("A new connection")
+          println(c)
+
+        case d @ Disconnected(_) =>
+          println("Disconnected")
+          println(d)
+
+        case m @ Error(exOpt) =>
+          System.err.println("ERROR: " + m)
+          exOpt foreach { _.printStackTrace(System.err) }
+
         case TextMessage(text) =>
-          println(text)
-          send(text)
+          println("Recieved text message: " + text)
+          send(text + ". We agree")
+
+        case m: JsonMessage =>
+          println("Json")
+          println(m)
+          println(pretty(render(m.content)))
+          send(m)
       }
     }
-  }).start
+  }
+
+  server.start
 }
